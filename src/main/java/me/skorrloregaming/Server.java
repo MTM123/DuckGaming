MTM123/DuckGaming

commit 6451a9112446f23d1a2bb0fd5710536443e7f611

Only notify about player not registering if player is cracked

src/main/java/me/skorrloregaming/listeners/PlayerEventHandler.java
@@ -1665,11 +1665,13 @@ public class PlayerEventHandler implements Listener {
 			player.leaveVehicle();
 		if ($.isPluginEnabled("AuthMe")) {
 			if (!Server.getPlugin().getConfig().contains("config." + player.getUniqueId().toString()) || !$.isAuthenticated(player)) {
-				String message = $.italicGray + "Player " + player.getName() + " has left without registering for this server";
-				Bukkit.broadcastMessage(message);
-				Server.getDiscordBot().broadcast(
-						ChatColor.stripColor(message.replace(player.getName(), "**" + player.getName() + "**"))
-						, Channel.SERVER_CHAT, Channel.SERVER_ACTIVITY);
+				if (!CraftGo.Player.getOnlineMode(player)) {
+					String message = $.italicGray + "Player " + player.getName() + " has left without registering for this server";
+					Bukkit.broadcastMessage(message);
+					Server.getDiscordBot().broadcast(
+							ChatColor.stripColor(message.replace(player.getName(), "**" + player.getName() + "**"))
+							, Channel.SERVER_CHAT, Channel.SERVER_ACTIVITY);
+				}
 			}
 		}
 		if (Server.getHubScoreboardTitleIndex().containsKey(player.getUniqueId()))

src/main/java/me/skorrloregaming/CraftGo.java
@@ -1690,6 +1690,14 @@ public class CraftGo {
 			}
 		}
 
+		public static boolean getOnlineMode(org.bukkit.entity.Player player) {
+			if (!$.isPluginEnabled("ProtocolSupport")) {
+				return Bukkit.getOnlineMode();
+			} else {
+				return protocolsupport.api.ProtocolSupportAPI.getConnection(player).getProfile().isOnlineMode();
+			}
+		}
+
 		public static void sendJson(final org.bukkit.entity.Player player, String json) {
 			try {
 				Packet.sendPacket(getPlayerConnection(player), Packet.Chat.newInstance(ChatSerializer.serializeJson(json)));

commit c01aaca62083f8896da7d4d54f79e1e32f563c53

Fix NullPointerException caused by commit 6451a9112446f..

src/main/java/me/skorrloregaming/Server.java
@@ -127,6 +127,8 @@ public class Server extends JavaPlugin implements Listener {
 	private static ConcurrentMap<Player, SignInfo> signEditParam = new ConcurrentHashMap<>();
 	private static ConcurrentMap<Integer, Integer> spawnerPrices = new ConcurrentHashMap<>();
 
+	private static ConcurrentMap<UUID, Boolean> onlineMode = new ConcurrentHashMap<>();
+
 	private static ArrayList<UUID> doubleJumpCandidates = new ArrayList<>();
 	private static ArrayList<UUID> staffChatPlayers = new ArrayList<>();
 	private static ArrayList<UUID> marriageChatPlayers = new ArrayList<>();
@@ -357,6 +359,10 @@ public class Server extends JavaPlugin implements Listener {
 
 	public static ConcurrentMap<Integer, Integer> getSpawnerPrices() {
 		return spawnerPrices;
+	}
+
+	public static ConcurrentMap<UUID, Boolean> getOnlineMode() {
+		return onlineMode;
 	}
 
 	public static class Kitpvp {

src/main/java/me/skorrloregaming/listeners/PlayerEventHandler.java
@@ -1368,6 +1368,7 @@ public class PlayerEventHandler implements Listener {
 	@EventHandler
 	public void onPlayerAuthenticate(PlayerAuthenticateEvent event) {
 		Player player = event.getPlayer();
+		Server.getOnlineMode().put(player.getUniqueId(), CraftGo.Player.getOnlineMode(player));
 		player.setWalkSpeed(0.2F);
 		player.setFlySpeed(0.1F);
 		Bukkit.getScheduler().runTaskAsynchronously(Server.getPlugin(), new Runnable() {
@@ -1665,7 +1666,7 @@ public class PlayerEventHandler implements Listener {
 			player.leaveVehicle();
 		if ($.isPluginEnabled("AuthMe")) {
 			if (!Server.getPlugin().getConfig().contains("config." + player.getUniqueId().toString()) || !$.isAuthenticated(player)) {
-				if (!CraftGo.Player.getOnlineMode(player)) {
+				if (!Server.getOnlineMode().getOrDefault(player.getUniqueId(), false)) {
 					String message = $.italicGray + "Player " + player.getName() + " has left without registering for this server";
 					Bukkit.broadcastMessage(message);
 					Server.getDiscordBot().broadcast(
@@ -1687,6 +1688,8 @@ public class PlayerEventHandler implements Listener {
 				)
 				, Channel.SERVER_CHAT, Channel.SERVER_ACTIVITY
 		);
+		if (Server.getOnlineMode().containsKey(player.getUniqueId()))
+			Server.getOnlineMode().remove(player.getUniqueId());
 	}
 
 	@EventHandler