35d03df756a0212a9db18dee04227d6c30d7871f
Speentie8081 <skorrloregaming.dev@gmail.com>
2019-01-11 10:59:32 -0600

    Update WorldEdit and WorldGuard dependency to fix SkyBlock

--- src/main/java/me/skorrloregaming/WorldGuardAPI.java
+++ src/main/java/me/skorrloregaming/WorldGuardAPI.java
@@ -1,54 +1,62 @@
 package me.skorrloregaming;
 
-import com.sk89q.worldedit.BlockVector2D;
-import com.sk89q.worldedit.Vector2D;
-import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.extension.platform.Platform;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.world.World;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.flags.Flags;
+import com.sk89q.worldguard.protection.managers.RegionManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 public class WorldGuardAPI {
 
-	public static Object getWorldGuard() {
-		return Bukkit.getPluginManager().getPlugin("WorldGuard");
+	public static WorldGuardPlugin getWorldGuard() {
+		return ((WorldGuardPlugin) Bukkit.getPluginManager().getPlugin("WorldGuard"));
 	}
 
-	public static Object getWorldEdit() {
-		return Bukkit.getPluginManager().getPlugin("WorldEdit");
+	public static WorldEditPlugin getWorldEdit() {
+		return ((WorldEditPlugin) Bukkit.getPluginManager().getPlugin("WorldEdit"));
 	}
 
-	public static Object getVectorFromLocation(Location location) {
-		return new com.sk89q.worldedit.Vector(location.getX(), location.getY(), location.getZ());
+	public static World getWorldByName(String worldName) {
+		for (Platform platform : WorldEdit.getInstance().getPlatformManager().getPlatforms()) {
+			for (World world : platform.getWorlds()) {
+				if (world.getName().equals(worldName))
+					return world;
+			}
+		}
+		throw new NullPointerException();
+	}
+
+	public static BlockVector3 getVectorFromLocation(Location location) {
+		return BlockVector3.at(location.getX(), location.getY(), location.getZ());
 	}
 
 	public static int getApplicableRegionsSetSize(Location location) {
-		Object platform = com.sk89q.worldguard.WorldGuard.getInstance().getPlatform();
-		Object world = ((com.sk89q.worldguard.WorldGuard.getInstance().getPlatform().getWorldByName(location.getWorld().getName())));
-		Object regionContainer = ((com.sk89q.worldguard.internal.platform.WorldGuardPlatform) platform).getRegionContainer();
-		Object regionManager = ((com.sk89q.worldguard.protection.regions.RegionContainer) regionContainer).get((com.sk89q.worldedit.world.World) world);
-		Object vector = getVectorFromLocation(location);
-		Object arSet = ((com.sk89q.worldguard.protection.managers.RegionManager) regionManager).getApplicableRegions((com.sk89q.worldedit.Vector) vector);
-		return ((com.sk89q.worldguard.protection.ApplicableRegionSet) arSet).size();
+		RegionManager regionManager = WorldGuard.getInstance().getPlatform().getRegionContainer().get(getWorldByName(location.getWorld().getName()));
+		BlockVector3 vector = getVectorFromLocation(location);
+		ApplicableRegionSet arSet = regionManager.getApplicableRegions(vector);
+		return arSet.size();
 	}
 
 	public static boolean testPassthroughRegionFlag(Player player, Location location) {
-		Object platform = com.sk89q.worldguard.WorldGuard.getInstance().getPlatform();
-		Object world = ((com.sk89q.worldguard.WorldGuard.getInstance().getPlatform().getWorldByName(location.getWorld().getName())));
-		Object regionContainer = ((com.sk89q.worldguard.internal.platform.WorldGuardPlatform) platform).getRegionContainer();
-		Object regionManager = ((com.sk89q.worldguard.protection.regions.RegionContainer) regionContainer).get((com.sk89q.worldedit.world.World) world);
-		Object vector = getVectorFromLocation(location);
-		Object arSet = ((com.sk89q.worldguard.protection.managers.RegionManager) regionManager).getApplicableRegions((com.sk89q.worldedit.Vector) vector);
-		return ((com.sk89q.worldguard.protection.ApplicableRegionSet) arSet).testState(((com.sk89q.worldguard.bukkit.WorldGuardPlugin) getWorldGuard()).wrapPlayer(player), com.sk89q.worldguard.protection.flags.Flags.PASSTHROUGH);
+		RegionManager regionManager = WorldGuard.getInstance().getPlatform().getRegionContainer().get(getWorldByName(location.getWorld().getName()));
+		BlockVector3 vector = getVectorFromLocation(location);
+		ApplicableRegionSet arSet = regionManager.getApplicableRegions(vector);
+		return arSet.testState(getWorldGuard().wrapPlayer(player), Flags.PASSTHROUGH);
 	}
 
 	public static boolean testBlockBreakRegionFlag(Player player, Location location) {
-		Object platform = com.sk89q.worldguard.WorldGuard.getInstance().getPlatform();
-		Object world = ((com.sk89q.worldguard.WorldGuard.getInstance().getPlatform().getWorldByName(location.getWorld().getName())));
-		Object regionContainer = ((com.sk89q.worldguard.internal.platform.WorldGuardPlatform) platform).getRegionContainer();
-		Object regionManager = ((com.sk89q.worldguard.protection.regions.RegionContainer) regionContainer).get((com.sk89q.worldedit.world.World) world);
-		Object vector = getVectorFromLocation(location);
-		Object arSet = ((com.sk89q.worldguard.protection.managers.RegionManager) regionManager).getApplicableRegions((com.sk89q.worldedit.Vector) vector);
-		return ((com.sk89q.worldguard.protection.ApplicableRegionSet) arSet).testState(((com.sk89q.worldguard.bukkit.WorldGuardPlugin) getWorldGuard()).wrapPlayer(player), com.sk89q.worldguard.protection.flags.Flags.BLOCK_BREAK);
+		RegionManager regionManager = WorldGuard.getInstance().getPlatform().getRegionContainer().get(getWorldByName(location.getWorld().getName()));
+		BlockVector3 vector = getVectorFromLocation(location);
+		ApplicableRegionSet arSet = regionManager.getApplicableRegions(vector);
+		return arSet.testState(getWorldGuard().wrapPlayer(player), Flags.BLOCK_BREAK);
 	}
 
 }
