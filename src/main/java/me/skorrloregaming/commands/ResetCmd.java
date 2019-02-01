c59c5586b57af8df38e70538e1c1dfbb96e4ce10
Speentie8081 <skorrloregaming.dev@gmail.com>
2019-02-01 16:02:36 -0600

    Add feature to safely backup and delete all player data for a minigame

--- src/main/java/me/skorrloregaming/commands/ResetCmd.java
+++ src/main/java/me/skorrloregaming/commands/ResetCmd.java
@@ -4,13 +4,36 @@ import me.skorrloregaming.$;
 import me.skorrloregaming.CraftGo;
 import me.skorrloregaming.Link$;
 import me.skorrloregaming.Server;
+import me.skorrloregaming.impl.ServerMinigame;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import java.io.File;
+
 public class ResetCmd implements CommandExecutor {
+
+	private int removeAllPlayerData(ServerMinigame minigame, File playerDataFolder, File altPlayerDataFolder, String subFolder) {
+		File dataFolder = new File(playerDataFolder, subFolder);
+		File[] listOfFiles = dataFolder.listFiles();
+		int deleted = 0;
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile()) {
+				if (listOfFiles[i].getName().contains(minigame.toString().toLowerCase())) {
+					File backupFolder = new File(altPlayerDataFolder, subFolder);
+					File backup = new File(altPlayerDataFolder, listOfFiles[i].getName());
+					backup.getParentFile().mkdirs();
+					if (backup.exists())
+						backup.delete();
+					listOfFiles[i].renameTo(backup);
+					deleted++;
+				}
+			}
+		}
+		return deleted;
+	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -22,16 +45,29 @@ public class ResetCmd implements CommandExecutor {
 			sender.sendMessage(Link$.Legacy.tag + ChatColor.GRAY + "Syntax " + ChatColor.RED + "/" + label + " <player>");
 			return true;
 		} else {
-			OfflinePlayer op = CraftGo.Player.getOfflinePlayer(args[0]);
-			if ((!op.hasPlayedBefore() && !op.isOnline()) || !Server.getPlugin().getConfig().contains("config." + op.getUniqueId().toString())) {
-				sender.sendMessage(Link$.Legacy.tag + ChatColor.RED + "Failed. " + ChatColor.GRAY + "The specified player could not be found.");
-				return true;
+			ServerMinigame minigame;
+			if ((minigame = ServerMinigame.valueOf(args[0].toUpperCase())) != null) {
+				File playerDataFolder = new File(Server.getPlugin().getDataFolder(), "Players");
+				File altPlayerDataFolder = new File(Server.getPlugin().getDataFolder(), "BackupPlayers");
+				int deleted = 0;
+				deleted += removeAllPlayerData(minigame, playerDataFolder, altPlayerDataFolder, "Chest");
+				deleted += removeAllPlayerData(minigame, playerDataFolder, altPlayerDataFolder, "Information");
+				deleted += removeAllPlayerData(minigame, playerDataFolder, altPlayerDataFolder, "Inventory");
+				deleted += removeAllPlayerData(minigame, playerDataFolder, altPlayerDataFolder, "Location");
+				deleted += removeAllPlayerData(minigame, playerDataFolder, altPlayerDataFolder, "PotionEffects");
+				sender.sendMessage(Link$.Legacy.tag + ChatColor.RED + "Success. " + ChatColor.GRAY + "Deleted " + ChatColor.RED + deleted + ChatColor.GRAY + " files.");
+			} else {
+				OfflinePlayer op = CraftGo.Player.getOfflinePlayer(args[0]);
+				if ((!op.hasPlayedBefore() && !op.isOnline()) || !Server.getPlugin().getConfig().contains("config." + op.getUniqueId().toString())) {
+					sender.sendMessage(Link$.Legacy.tag + ChatColor.RED + "Failed. " + ChatColor.GRAY + "The specified player could not be found.");
+					return true;
+				}
+				String msg = ChatColor.RED + "You have been forcibly unregistered from the server.";
+				if (op.isOnline())
+					op.getPlayer().kickPlayer(msg);
+				Server.getPlugin().getConfig().set("config." + op.getUniqueId().toString(), null);
+				sender.sendMessage(Link$.Legacy.tag + ChatColor.RED + "Success. " + ChatColor.GRAY + "The specified player should now be unregistered.");
 			}
-			String msg = ChatColor.RED + "You have been forcibly unregistered from the server.";
-			if (op.isOnline())
-				op.getPlayer().kickPlayer(msg);
-			Server.getPlugin().getConfig().set("config." + op.getUniqueId().toString(), null);
-			sender.sendMessage(Link$.Legacy.tag + ChatColor.RED + "Success. " + ChatColor.GRAY + "The specified player should now be unregistered.");
 		}
 		return true;
 	}
